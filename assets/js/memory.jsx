887dfd0bf245d369149260822a639ed22aa717d9
tina-n219 <noorani.t@husky.neu.edu>
2018-09-25 13:12:54 -0400

    memory

---
+++ assets/js/memory.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import _ from 'lodash';
+
+export default function game_init(root) {
+    ReactDOM.render(<Starter />, root);
+  }
+
+  class Board extends React.Component {
+      
+  }
