1f1308152af3cd254317e1f0ad9d39e0fbf7380c
tina-n219 <noorani.t@husky.neu.edu>
2018-09-25 14:26:44 -0400

    experimenting

--- assets/js/memory.jsx
+++ assets/js/memory.jsx
@@ -3,9 +3,33 @@ import ReactDOM from 'react-dom';
 import _ from 'lodash';
 
 export default function game_init(root) {
-    ReactDOM.render(<Starter />, root);
+    ReactDOM.render(<Board />, root);
+  }
+
+  class Card extends React.Component {
+      constructor(props) {
+          super(props)
+          this.state = {};
+      }
   }
 
   class Board extends React.Component {
-      
+    constructor(props) {
+        super(props)
+        this.state = { score: 0, 
+                       gameOver: false };
+    }
+
+    render() {
+
+        let score = <div className="column">
+        <p>Score: {this.state.score}</p>
+        <p>Game Over: {this.state.gameOver}</p>
+      </div>;
+
+        return <div className="row">
+        {score}
+      </div>
+        ;
+    }
   }
