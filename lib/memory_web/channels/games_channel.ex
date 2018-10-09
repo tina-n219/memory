5398976643e4e2fb8099db0ab7adc82c02fefc97
tina-n219 <noorani.t@husky.neu.edu>
2018-10-09 13:59:08 -0400

    channel

--- lib/memory_web/channels/games_channel.ex
+++ lib/memory_web/channels/games_channel.ex
@@ -6,11 +6,11 @@ defmodule MemoryWeb.GamesChannel do
 
   def join("games:" <> name, payload, socket) do
     if authorized?(payload) do
-      game = Game.new()
+      game = State.new()
       socket = socket
       |> assign(:game, game)
       |> assign(:name, name)
-      {:ok, %{"join" => name, "game" => Game.client_view(game)}, socket}
+      {:ok, %{"join" => name, "game" => State.client_view(game)}, socket}
     else
       {:error, %{reason: "unauthorized"}}
     end
@@ -25,17 +25,17 @@ defmodule MemoryWeb.GamesChannel do
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (games:lobby).
   def handle_in("selectCard", %{"index" => ii}, socket) do
-    game = Game.guess(socket.assigns[:game], ii)
+    game = State.guess(socket.assigns[:game], ii)
     socket = assign(socket, :game, game)
-    {:reply, {:ok, %{ "game" => Game.client_view(game)}}, socket}
+    {:reply, {:ok, %{ "game" => State.client_view(game)}}, socket}
   end
 
-  def handle_in("restart", payload, socket) do
-    game = State.new()
-    socket = assign(socket, :game, game)
-    BackupAgent.put(socket.assigns[:name], game)
-    {:reply, {:ok, %{"game" => State.client_view(game)}}, socket}
-  end
+  # def handle_in("restart", payload, socket) do
+  #   game = State.new()
+  #   socket = assign(socket, :game, game)
+  #   BackupAgent.put(socket.assigns[:name], game)
+  #   {:reply, {:ok, %{"game" => State.client_view(game)}}, socket}
+  # end
 
   # Add authorization logic here as required.
   defp authorized?(_payload) do
--- lib/memory_web/router.ex
+++ lib/memory_web/router.ex
@@ -19,6 +19,7 @@ defmodule MemoryWeb.Router do
 
     get "/", PageController, :index
     get "/game/:game", PageController, :game
+    post "/join", PageController, :join
   end
 
   # Other scopes may use custom stacks.
