b3037038f5325d390df9afd37957a5b3500265a9
tina-n219 <noorani.t@husky.neu.edu>
2018-10-09 13:39:45 -0400

    plugs, router, controller

--- lib/memory_web/controllers/page_controller.ex
+++ lib/memory_web/controllers/page_controller.ex
@@ -5,7 +5,20 @@ defmodule MemoryWeb.PageController do
     render conn, "index.html"
   end
 
+  def join(conn, %{"join" => %{"user" => user, "game" => game}}) do
+    conn
+    |> put_session(:user, user)
+    |> redirect(to: "/game/#{game}")
+  end
+
   def game(conn, params) do
-    render conn, "game.html", game: params["game"]
+    user = get_session(conn, :user)
+    if user do
+      render conn, "game.html", game: params["game"], user: user
+    else
+      conn
+      |> put_flash(:error, "Must pick a username")
+      |> redirect(to: "/")
+    end
   end
 end
--- lib/memory_web/router.ex
+++ lib/memory_web/router.ex
@@ -7,6 +7,7 @@ defmodule MemoryWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug MemoryWeb.Plugs.PutUserToken
   end
 
   pipeline :api do
